604d6f0b5e6b5ff8031c7addb353c97426255994
WonderfulVoid <ola.liljedahl@gmail.com>
2015-03-09 00:00:51 +0100

    Re-indent and add braces to position.c

--- position.c
+++ position.c
@@ -20,8 +20,14 @@ static void write_board(FILE *file,board brade)
 		for(col=0;col<8;col++)
 		{
 			char c='.';
-			if(GETBIT(brade.black,8*row+col)) c='x';
-			else if(GETBIT(brade.white,8*row+col)) c='o';
+			if(GETBIT(brade.black,8*row+col))
+			{
+				c='x';
+			}
+			else if(GETBIT(brade.white,8*row+col))
+			{
+				c='o';
+			}
 			fputc(c,file);
 		}
 		fputc('\n',file);
@@ -38,11 +44,20 @@ STATIC bool save_position(char *filename,position_t *pos)
 		{
 			int len;
 			printf("Enter filename: "); fflush(stdout);
-			if(!fgets(fname,sizeof fname,stdin)) return false; /* end-of-file or error */
+			if(!fgets(fname,sizeof fname,stdin))
+			{
+				return false; /* end-of-file or error */
+			}
 			/* remove stupid newline from end of string */
 			len=strlen(fname);
-			if(len>0 && fname[len-1]=='\n') fname[--len]=0;
-			if(len==0) return false;
+			if(len>0 && fname[len-1]=='\n')
+			{
+				fname[--len]=0;
+			}
+			if(len==0)
+			{
+				return false;
+			}
 			filename=fname;
 		}
 		file=fopen(filename,"w");
@@ -52,21 +67,37 @@ STATIC bool save_position(char *filename,position_t *pos)
 			fprintf(file,"board\n");
 			write_board(file,pos->brade);
 			fprintf(file,"timestamp %lu\n",pos->timestamp);
-			fprintf(file,"computercolor %s\n",pos->computer==black?"black":"white");
+			fprintf(file,"computercolor %s\n",
+					pos->computer==black?"black":"white");
 			fprintf(file,"humantime %d\n",pos->humantime);
 			fprintf(file,"computertime %d\n",pos->computertime);
 			fprintf(file,"targettime %d\n",pos->targettime);
-			fprintf(file,"turntomove %s\n",pos->turntomove==black?"black":"white");
+			fprintf(file,"turntomove %s\n",
+					pos->turntomove==black?"black":"white");
 			fprintf(file,"passes %d\n",pos->passes);
 			fprintf(file,"cachesize %lu\n",pos->cachesize);
-			fprintf(file,"deterministic %s\n",pos->deterministic?"true":"false");
-			if(strlen(pos->computername)) fprintf(file,"computername %s\n",pos->computername);
-			if(strlen(pos->humanname)) fprintf(file,"humanname %s\n",pos->humanname);
+			fprintf(file,"deterministic %s\n",
+					pos->deterministic?"true":"false");
+			if(strlen(pos->computername))
+			{
+				fprintf(file,"computername %s\n",pos->computername);
+			}
+			if(strlen(pos->humanname))
+			{
+				fprintf(file,"humanname %s\n",pos->humanname);
+			}
 			fprintf(file,"moves ");
 			for(i=0;i<pos->nummoves;i++)
 			{
-				if(pos->moves[i]<64) fprintf(file,"%c%c ",pos->moves[i]%8+'a',pos->moves[i]/8+'1');
-				else fprintf(file,"ps ");
+				if(pos->moves[i]<64)
+				{
+					fprintf(file,"%c%c ",
+							pos->moves[i]%8+'a',pos->moves[i]/8+'1');
+				}
+				else
+				{
+					fprintf(file,"ps ");
+				}
 			}
 			fprintf(file,"\n");
 			fprintf(file,"times ");
@@ -81,21 +112,31 @@ STATIC bool save_position(char *filename,position_t *pos)
 				for(;;)
 				{
 					const char *str = "";
-					while(pos->eval[i]==eval && i<60) i++;
-					if(i==60) break;
+					while(pos->eval[i]==eval && i<60)
+					{
+						i++;
+					}
+					if(i==60)
+					{
+						break;
+					}
 					eval=pos->eval[i];
 					switch(eval)
 					{
-						case discdiff : str="discdiff";
-										break;
-						case winloss : str="winloss";
-										break;
-						case mobility : str="mobility";
-										break;
-						case potmobility : str="potmobility";
-										break;
+						case discdiff :
+							str="discdiff";
+							break;
+						case winloss :
+							str="winloss";
+							break;
+						case mobility :
+							str="mobility";
+							break;
+						case potmobility :
+							str="potmobility";
+							break;
 						case dummyeval:
-										break;
+							break;
 					}
 					fprintf(file,"%s %d\n",str,i+1);
 				}
@@ -107,7 +148,10 @@ STATIC bool save_position(char *filename,position_t *pos)
 			}
 			return true;
 		}
-		else fprintf(stderr,"errno %d opening file %s\n",errno,filename);
+		else
+		{
+			fprintf(stderr,"errno %d opening file %s\n",errno,filename);
+		}
 		filename=0;
 	}
 }
@@ -161,9 +205,18 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 	pos->nummoves=0;
 	pos->targettime=20*60; /* 20 minutes, 60 secs/min */
 	pos->computer=white;
-	while(i<44) pos->eval[i++]=potmobility;
-	while(i<48) pos->eval[i++]=winloss;
-	while(i<60) pos->eval[i++]=discdiff;
+	while(i<44)
+	{
+		pos->eval[i++]=potmobility;
+	}
+	while(i<48)
+	{
+		pos->eval[i++]=winloss;
+	}
+	while(i<60)
+	{
+		pos->eval[i++]=discdiff;
+	}
 	pos->cachesize=0;
 	pos->deterministic=false;
 	strncpy(pos->computername,COMPUTERNAME,sizeof pos->computername);
@@ -178,11 +231,20 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 		{
 			int len;
 			printf("Enter filename: "); fflush(stdout);
-			if(!fgets(fname,sizeof fname,stdin)) return false; /* end-of-file or error */
+			if(!fgets(fname,sizeof fname,stdin))
+			{
+				return false; /* end-of-file or error */
+			}
 			/* remove stupid newline from end of string */
 			len=strlen(fname);
-			if(len>0 && fname[len-1]=='\n') fname[--len]=0;
-			if(len==0) return false;
+			if(len>0 && fname[len-1]=='\n')
+			{
+				fname[--len]=0;
+			}
+			if(len==0)
+			{
+				return false;
+			}
 			filename=fname;
 		}
 		file=fopen(filename,"r");
@@ -198,7 +260,8 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 				(void)rc;
 				if(ferror(file))
 				{
-					fprintf(stderr,"error: could not read %sfile %s errno %d\n",parameters?"parameter ":"",filename,errno);
+					fprintf(stderr,"error: could not read %sfile %s errno %d\n",
+							parameters?"parameter ":"",filename,errno);
 					break;
 				}
 				else if(feof(file))
@@ -207,22 +270,35 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 					break;
 				}
 				len=strlen(buf);
-				if(len>0 && buf[len-1]=='\n') buf[--len]=0;
+				if(len>0 && buf[len-1]=='\n')
+				{
+					buf[--len]=0;
+				}
 				if(!parameters && strncmp(buf,"timestamp ",10)==0)
 				{
 					pos->timestamp=atol(buf+10);
 				}
 				else if(!parameters && strncmp(buf,"board",5)==0)
 				{
-					if(!read_board(file,&pos->brade)) break;
+					if(!read_board(file,&pos->brade))
+					{
+						break;
+					}
 				}
 				else if(strncmp(buf,"computercolor ",14)==0)
 				{
-					if(strncmp(buf+14,"black",5)==0) pos->computer=black;
-					else if(strncmp(buf+14,"white",5)==0) pos->computer=white;
+					if(strncmp(buf+14,"black",5)==0)
+					{
+						pos->computer=black;
+					}
+					else if(strncmp(buf+14,"white",5)==0)
+					{
+						pos->computer=white;
+					}
 					else
 					{
-						fprintf(stderr,"error: invalid computer color %s\n",buf+14);
+						fprintf(stderr,"error: invalid computer color %s\n",
+								buf+14);
 						break;
 					}
 				}
@@ -240,11 +316,18 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 				}
 				else if(!parameters && strncmp(buf,"turntomove ",11)==0)
 				{
-					if(strncmp(buf+11,"black",5)==0) pos->turntomove=black;
-					else if(strncmp(buf+11,"white",5)==0) pos->turntomove=white;
+					if(strncmp(buf+11,"black",5)==0)
+					{
+						pos->turntomove=black;
+					}
+					else if(strncmp(buf+11,"white",5)==0)
+					{
+						pos->turntomove=white;
+					}
 					else
 					{
-						fprintf(stderr,"error: invalid turntomove color %s\n",buf+11);
+						fprintf(stderr,"error: invalid turntomove color %s\n",
+								buf+11);
 						break;
 					}
 				}
@@ -258,11 +341,18 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 				}
 				else if(strncmp(buf,"deterministic ",14)==0)
 				{
-					if(strcmp(buf+14,"true")==0) pos->deterministic=true;
-					else if(strcmp(buf+14,"false")==0) pos->deterministic=false;
+					if(strcmp(buf+14,"true")==0)
+					{
+						pos->deterministic=true;
+					}
+					else if(strcmp(buf+14,"false")==0)
+					{
+						pos->deterministic=false;
+					}
 					else
 					{
-						fprintf(stderr,"error: invalid deterministic truth value %s\n",buf+9);
+						fprintf(stderr,"error: invalid deterministic truth value %s\n",
+								buf+9);
 						break;
 					}
 				}
@@ -282,8 +372,13 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 					char *p;
 					for(p=buf+6;*p;p+=3)
 					{
-						if(p[0]=='p' && p[1]=='s' && p[2]==' ') pos->moves[i++]=64;
-						else if(p[0]>='a' && p[0]<='h' && p[1]>='1' && p[1]<='8' && p[2]==' ')
+						if(p[0]=='p' && p[1]=='s' && p[2]==' ')
+						{
+							pos->moves[i++]=64;
+						}
+						else if(p[0]>='a' && p[0]<='h' &&
+								p[1]>='1' && p[1]<='8' &&
+								p[2]==' ')
 						{
 							pos->moves[i++]=p[0]-'a'+8*(p[1]-'1');
 						}
@@ -302,33 +397,52 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 					while(*p)
 					{
 						pos->times[i++]=(short)atoi(p);
-						while(*p && !isspace(*p)) p++;
-						while(*p && isspace(*p)) p++;
+						while(*p && !isspace(*p))
+						{
+							p++;
+						}
+						while(*p && isspace(*p))
+						{
+							p++;
+						}
 					}
 				}
 				else if(strncmp(buf,"winloss ",8)==0)
 				{	
 					int i=atoi(buf+8)-1;
-					while(i<60) pos->eval[i++]=winloss;
+					while(i<60)
+					{
+						pos->eval[i++]=winloss;
+					}
 				}
 				else if(strncmp(buf,"discdiff ",9)==0)
 				{
 					int i=atoi(buf+9)-1;
-					while(i<60) pos->eval[i++]=discdiff;
+					while(i<60)
+					{
+						pos->eval[i++]=discdiff;
+					}
 				}
 				else if(strncmp(buf,"mobility ",9)==0)
 				{
 					int i=atoi(buf+9)-1;
-					while(i<60) pos->eval[i++]=mobility;
+					while(i<60)
+					{
+						pos->eval[i++]=mobility;
+					}
 				}
 				else if(strncmp(buf,"potmobility ",12)==0)
 				{
 					int i=atoi(buf+12)-1;
-					while(i<60) pos->eval[i++]=potmobility;
+					while(i<60)
+					{
+						pos->eval[i++]=potmobility;
+					}
 				}
 				else if(buf[0]!='#')
 				{
-					fprintf(stderr,"error: unrecognized identifier '%s' in %sfile '%s'\n",buf,parameters?"parameter ":"",filename);
+					fprintf(stderr,"error: unrecognized identifier '%s' in %sfile '%s'\n",
+							buf,parameters?"parameter ":"",filename);
 					break;
 				}
 			}
@@ -337,10 +451,12 @@ STATIC bool load_position(char *filename,position_t *pos,bool parameters)
 		}
 		else if(parameters)
 		{
-			fprintf(stderr,"warning: could not open parameterfile '%s' errno %d\n",filename,errno);
+			fprintf(stderr,"warning: could not open parameterfile '%s' errno %d\n",
+					filename,errno);
 			return false;
 		}
-		fprintf(stderr,"error: could not open savefile '%s' errno %d\n",filename,errno);
+		fprintf(stderr,"error: could not open savefile '%s' errno %d\n",
+				filename,errno);
 		filename=0; /* ask user for alternative filename */
 	}
 }
